b6138b65c2c883bff7a93b4fec43f66a951c0bee
Dylan <klj40702@gmail.com>
2024-11-14 10:58:50 +0800

    msw.enable

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
+const IS_ENABLE_MSW = process.env.ENABLE_MSW === 'true'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@crazydos/nuxt-msw'],
+  msw: {
+    enable: IS_ENABLE_MSW,
+  },
 })
